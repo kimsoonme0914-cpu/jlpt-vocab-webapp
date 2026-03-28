ba3ccdfdf3c6ecdcf7b533896c4d62919f7d04ab
kimsoonme0914-cpu <kimsoonme0914@gmail.com>
2026-03-29 01:53:34 +0900

    Update vocabPart3.js

--- src/vocabPart3.js
+++ src/vocabPart3.js
@@ -1,1 +1,35 @@
-
+export const PART3 = {
+  title: "N5-N3 가타카나어",
+  days: {
+    46: { theme: "일상생활", words: [
+      {kanji:"ポケット",reading:"ポケット",meaning:"주머니, 포켓"},{kanji:"スカート",reading:"スカート",meaning:"스커트, 치마"},{kanji:"サイズ",reading:"サイズ",meaning:"사이즈"},{kanji:"シャツ",reading:"シャツ",meaning:"셔츠"},{kanji:"ズボン",reading:"ズボン",meaning:"바지"},
+      {kanji:"パーティー",reading:"パーティー",meaning:"파티"},{kanji:"キッチン",reading:"キッチン",meaning:"주방"},{kanji:"シャワー",reading:"シャワー",meaning:"샤워"},{kanji:"テーブル",reading:"テーブル",meaning:"테이블"},{kanji:"エアコン",reading:"エアコン",meaning:"에어컨"},
+      {kanji:"カッター",reading:"カッター",meaning:"커터 칼"},{kanji:"カット",reading:"カット",meaning:"자름, 커트"},{kanji:"メートル",reading:"メートル",meaning:"미터"},{kanji:"パンフレット",reading:"パンフレット",meaning:"팸플릿"},{kanji:"イメージ",reading:"イメージ",meaning:"이미지, 인상"},
+      {kanji:"ハンカチ",reading:"ハンカチ",meaning:"손수건"},{kanji:"レジ",reading:"レジ",meaning:"계산대"},{kanji:"ベッド",reading:"ベッド",meaning:"침대"},{kanji:"ノック",reading:"ノック",meaning:"노크"},{kanji:"ペット",reading:"ペット",meaning:"반려동물"}
+    ]},
+    47: { theme: "음식, 장소", words: [
+      {kanji:"バナナ",reading:"バナナ",meaning:"바나나"},{kanji:"ケーキ",reading:"ケーキ",meaning:"케이크"},{kanji:"カレー",reading:"カレー",meaning:"카레"},{kanji:"コーヒー",reading:"コーヒー",meaning:"커피"},{kanji:"チョコレート",reading:"チョコレート",meaning:"초콜릿"},
+      {kanji:"レストラン",reading:"レストラン",meaning:"레스토랑"},{kanji:"ビル",reading:"ビル",meaning:"빌딩, 건물"},{kanji:"アパート",reading:"アパート",meaning:"아파트"},{kanji:"キャンセル",reading:"キャンセル",meaning:"취소, 캔슬"},{kanji:"アルコール",reading:"アルコール",meaning:"알코올"},
+      {kanji:"タクシー",reading:"タクシー",meaning:"택시"},{kanji:"トイレ",reading:"トイレ",meaning:"화장실"},{kanji:"ストップ",reading:"ストップ",meaning:"멈춤, 정지"},{kanji:"プール",reading:"プール",meaning:"수영장"},{kanji:"オープン",reading:"オープン",meaning:"오픈"},
+      {kanji:"クッキー",reading:"クッキー",meaning:"쿠키"},{kanji:"オフィス",reading:"オフィス",meaning:"사무실"},{kanji:"エンジン",reading:"エンジン",meaning:"엔진"},{kanji:"ジュース",reading:"ジュース",meaning:"주스"},{kanji:"カーブ",reading:"カーブ",meaning:"커브, 곡선"}
+    ]},
+    48: { theme: "학업, 업무", words: [
+      {kanji:"コミュニケーション",reading:"コミュニケーション",meaning:"커뮤니케이션"},{kanji:"ヒント",reading:"ヒント",meaning:"힌트"},{kanji:"マーク",reading:"マーク",meaning:"마크, 상표"},{kanji:"アンケート",reading:"アンケート",meaning:"앙케트, 설문"},{kanji:"ユーモア",reading:"ユーモア",meaning:"유머"},
+      {kanji:"チャンス",reading:"チャンス",meaning:"기회, 찬스"},{kanji:"スケジュール",reading:"スケジュール",meaning:"스케줄, 일정"},{kanji:"メッセージ",reading:"メッセージ",meaning:"메시지"},{kanji:"アルバイト",reading:"アルバイト",meaning:"아르바이트"},{kanji:"サイン",reading:"サイン",meaning:"사인, 서명"},
+      {kanji:"アドバイス",reading:"アドバイス",meaning:"조언, 충고"},{kanji:"スピーチ",reading:"スピーチ",meaning:"스피치, 연설"},{kanji:"インタビュー",reading:"インタビュー",meaning:"인터뷰"},{kanji:"サービス",reading:"サービス",meaning:"서비스"},{kanji:"アイデア",reading:"アイデア",meaning:"아이디어"},
+      {kanji:"スタッフ",reading:"スタッフ",meaning:"직원, 스태프"},{kanji:"メール",reading:"メール",meaning:"메일"},{kanji:"コピー",reading:"コピー",meaning:"복사"},{kanji:"レポート",reading:"レポート",meaning:"리포트, 보고서"},{kanji:"テキスト",reading:"テキスト",meaning:"교재, 교과서"}
+    ]},
+    49: { theme: "환경, 취미", words: [
+      {kanji:"タイトル",reading:"タイトル",meaning:"제목, 타이틀"},{kanji:"デート",reading:"デート",meaning:"데이트"},{kanji:"パソコン",reading:"パソコン",meaning:"PC, 퍼스널 컴퓨터"},{kanji:"リサイクル",reading:"リサイクル",meaning:"재활용"},{kanji:"ギター",reading:"ギター",meaning:"기타"},
+      {kanji:"コンサート",reading:"コンサート",meaning:"콘서트"},{kanji:"デザイン",reading:"デザイン",meaning:"디자인"},{kanji:"レジャー",reading:"レジャー",meaning:"레저"},{kanji:"テーマ",reading:"テーマ",meaning:"테마, 주제"},{kanji:"チャレンジ",reading:"チャレンジ",meaning:"도전, 챌린지"},
+      {kanji:"グループ",reading:"グループ",meaning:"그룹"},{kanji:"ロッカー",reading:"ロッカー",meaning:"보관함, 로커"},{kanji:"カメラ",reading:"カメラ",meaning:"카메라"},{kanji:"コンピューター",reading:"コンピューター",meaning:"컴퓨터"},{kanji:"チェック",reading:"チェック",meaning:"체크, 확인"},
+      {kanji:"インターネット",reading:"インターネット",meaning:"인터넷"},{kanji:"チケット",reading:"チケット",meaning:"티켓"},{kanji:"ゲーム",reading:"ゲーム",meaning:"게임"},{kanji:"ツアー",reading:"ツアー",meaning:"투어, 여행"},{kanji:"プレゼント",reading:"プレゼント",meaning:"선물"}
+    ]},
+    50: { theme: "운동 및 기타", words: [
+      {kanji:"スポーツ",reading:"スポーツ",meaning:"스포츠, 운동"},{kanji:"スイッチ",reading:"スイッチ",meaning:"스위치"},{kanji:"アメリカ",reading:"アメリカ",meaning:"미국"},{kanji:"セット",reading:"セット",meaning:"세트, 세팅"},{kanji:"ニュース",reading:"ニュース",meaning:"뉴스"},
+      {kanji:"ルール",reading:"ルール",meaning:"규칙, 룰"},{kanji:"トレーニング",reading:"トレーニング",meaning:"트레이닝, 훈련"},{kanji:"アップ",reading:"アップ",meaning:"업, 상승"},{kanji:"スリッパ",reading:"スリッパ",meaning:"슬리퍼"},{kanji:"スタート",reading:"スタート",meaning:"시작"},
+      {kanji:"エネルギー",reading:"エネルギー",meaning:"에너지"},{kanji:"ヨーロッパ",reading:"ヨーロッパ",meaning:"유럽"},{kanji:"エレベーター",reading:"エレベーター",meaning:"엘리베이터"},{kanji:"カロリー",reading:"カロリー",meaning:"칼로리"},{kanji:"オーバー",reading:"オーバー",meaning:"오버, 초과"},
+      {kanji:"エスカレーター",reading:"エスカレーター",meaning:"에스컬레이터"},{kanji:"サポート",reading:"サポート",meaning:"서포트, 지원"},{kanji:"スキー",reading:"スキー",meaning:"스키"},{kanji:"ジョギング",reading:"ジョギング",meaning:"조깅"},{kanji:"マラソン",reading:"マラソン",meaning:"마라톤"}
+    ]}
+  }
+};
